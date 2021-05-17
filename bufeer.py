c14d2e720122f1f3988b56203577af7fbb3343a9
bokonV2 <bokon2014@yandex.ru>
2021-05-17 21:59:29 +0300

    Start

---
+++ bufeer.py
@@ -0,0 +1,110 @@
+from keyboard import on_release_key, send, write, wait, remove_hotkey, add_hotkey, remap_hotkey, unhook_all_hotkeys
+import time
+import win32com.client
+shell = win32com.client.Dispatch('WScript.Shell')
+import pynput
+
+
+class Buff():
+
+    def __init__(self):
+        self.buff0 = ""
+        self.buff1 = ""
+        self.buff2 = ""
+        self.hotkey = "ctrl + b"
+        self.hotkey2 = "ctrl + n"
+        self.hotkey3 = "ctrl + m"
+        self.hotkey4 = "m + n"
+        self.whait = 0.5
+        self.listHotkey = [[self.hotkey,self.pastBuff0], [self.hotkey2,self.pastBuff1],[self.hotkey3,self.pressMouse], [self.hotkey4, self.pastLine]]
+
+
+    def pastBuff0(self):
+            write(self.buff0, delay=0.001)
+            send('enter')
+
+    def pastBuff1(self):
+            write(self.buff1, delay=0.001)
+            send('enter')
+
+    def pastLine(self):
+        line = self.buff2.split('.')
+        for i in line:
+            write(i, delay=0.001)
+            send('enter')
+            time.sleep(self.whait)
+
+    def AddHotKey(self):
+        print("Запуск скрипта")
+        self.liveWork = True
+        for i in self.listHotkey:
+            add_hotkey(i[0], i[-1])
+
+    def RemapHotKey(self, hotkey = None, hotkey2 = None, hotkey3 = None, hotkey4 = None):
+        try:
+            self.RemoveHotKey()
+        except:
+            pass
+
+        if hotkey != None:
+            self.hotkey = hotkey
+
+        if hotkey2 != None:
+            self.hotkey2 = hotkey2
+
+        if hotkey3 != None:
+            self.hotkey3 = hotkey3
+
+        if hotkey4 != None:
+            self.hotkey4 = hotkey4
+
+        self.listHotkey = [[self.hotkey,self.pastBuff0], [self.hotkey2,self.pastBuff1],[self.hotkey3,self.pressMouse], [self.hotkey4, self.pastLine]]
+
+        #print(self.hotkey, self.hotkey2, self.hotkey3)
+
+        if len(self.hotkey) == 1: self.RemapKey(self.hotkey)
+        if len(self.hotkey2) == 1: self.RemapKey(self.hotkey2)
+        if len(self.hotkey3) == 1: self.RemapKey(self.hotkey3)
+        if len(self.hotkey4) == 1: self.RemapKey(self.hotkey4)
+
+
+    def RemapKey(self,key):
+        remap_hotkey(key, 'ctrl+{}'.format(key))
+
+    def RemoveHotKey(self):
+        for i in self.listHotkey:
+            remove_hotkey(i[0])
+        print("Остановка скрипта")
+        unhook_all_hotkeys()
+
+    def Wait(self):
+        wait("Ctrl + q")
+        print(self.x)
+        print(self.y)
+
+    def on_click(self, x, y, button, pressed):
+        print('{0} at {1}'.format('Pressed' if pressed else 'Released',(x, y)))
+        self.x = x
+        self.y = y
+        if not pressed:
+            return False
+
+    # Collect events until released Button, Controller
+    def Mouse(self):
+        with pynput.mouse.Listener(on_click=self.on_click) as listener:
+            listener.join()
+
+    def pressMouse(self):
+        controller = pynput.mouse.Controller()
+        controller.position = (self.x, self.y)
+        controller.click(pynput.mouse.Button.left, 1)
+
+
+#time.sleep(5)
+#time.sleep(5)
+#Buff().pasrLine()
+#a = Buff()
+#a.AddHotKey()
+#a.getNewBuff()
+#a.pastNewBuff()
+#a.Wait()
---
+++ main.py
@@ -0,0 +1,167 @@
+icon = "iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAABGdBTUEAALGPC/xhBQAAAAFzUkdCAK7OHOkAAAAgY0hSTQAAeiYAAICEAAD6AAAAgOgAAHUwAADqYAAAOpgAABdwnLpRPAAAAtNQTFRFAAAAICMoHyMnHiEmIiUkHyIoICQoISMnICMnHB8mICInICMqHyInICMoICMoICMoICMoICMoICMoICMoHyMnICMoICMoICMoICMoICMoICIoICMoICMoICMoICMoICMoICMoICMoICMoICMoICMoICMoICMoICMoICMoICMoICMoHyMnHyInICMoICMoICMoICMoHyMoICMoICMoICMoICMoICMoHyIoICMoICMoICMoICMoICMoICMoICMoICMoICMoICInICMoHyMoICMoICMoHyInHSAlLC80LC4zMDI3dXZ6yMnKx8jJdXd6MTQ5eXp9zc3O+vr7////zc7Pent+MTQ4HiEmNTc8foCDz9DR+/v7zs/QMzY6ODs/goSH09TV/Pz80dLTNzo+T1JW0tLT/v7+xsfIPkBFLjE1bnB0wMHC+fn58PDxra+wXmBkJikuHB8lKy4zamxvvr/A9/f47u/vqqutVlhcJCcrGx4kKiwxZ2ltvb6/9vf36erqoaKkUVNXIyYrHB8kHiInMjU5Gx4jKSwwZGZqvLy+9fX15ubmmpyeSk1RISQpMzU6LzI2OjxBkZKV1tbX1dXWhoiLOTxAKSwxVVdbREZLQURIk5SX19jZ0dHSg4WHLTA1RklNuru89/f3/f392trbh4mLQ0ZKlZaZ4OHh8PDwpKWnKCswX2Fltba49PT02tvciIqMPD5DRUdLl5ia4+Pk5+jonp+iT1FVJykuXV9jsrO08vPz3+Dg5ebmmJmcSEpOJSgtWlxgrrCx8fHx4ODhkJGUREdLHSEmHSAmJCcsWFpenZ+hQENHSUxQiYuNPkFFTlFV1dbX4uPjlpiaREZKTE5SnqCiODtA7+/w4+Tkl5iboqOl6enq5OTllZeZU1VZp6iqnp+h6+vs3d7fjY+RIyYqUVRYo6Wn7e3uiYqNPUBEIiUqUFNWoKKk6OnpgoSGNTg8HIa7LAAAAEV0Uk5TAAAAAAAAAAAAAAAAAAMqcq3V9PMDOZjf/DgDG4rqGTbC/r8yRtnWQ9jBwAICOukCkwIr3mylawKGGr4z1Nc/NTEDNwOmJvfrmAAAAAFiS0dEU3pnHQYAAAAJcEhZcwAACxMAAAsTAQCanBgAAAKHSURBVDjLY2CAAkZePn4BQSFXV2FBAX4+XkYGVMAkIiomLuEKBRLiYpJSyEqYGaVlZF1RgKyMHCMLTJ6VTV7BFR0oKikzskPlGVVU4eJubnCmmjojB8R8eQ24tLuHpztciZo8IyfI+dKacGkvbx9fPy+4Ei1poEsZtWVg0v4BgUHBwUEhoWEwJTI6XAyMumD3h7tHREZFB4NBdExkLESJniQjg74YWG1cfEJiMBwkJsUng4UNDBmMxEH6U1LTglFAWnpGOFDC2IiBHxR+bplZ2TnI8rl5+QXgMDVhEIC4sLCouKQUJl1WXlHpDnGmKYMg1EvhVdU1tXUg6fqGxqZmmE8FGYTBdEt4q797W3tHZ1d3T29fm1v/hHCIEmEGiA0TJ02eMnWae9v0GTNd28JnzZ4zd958iAqIgvAFCxcFL16ydKJ7W7PbsuUrgoNXroqAKoBYEb56zdp1wYvXb9i4afOW4OCt27Y3wawQhAX0jp27dgcv3rM3OHjvvv2u7nBHCkA9Ee7eduDgocPBaUeOHnOrOn4iHOZNcEC5Ni04enRZeNvJU6c3nAlvcz07+9z5PnBAmUGD+sLF4OBLl6+4tbm5V7levbY1OC37uhskqCGRVVBRfiM4+OatY25922/fCQ6+e29/EziyzBkYRfVALPem+w/KgBIPHzwKTnv85IpbOCy6GfWhCcb96bPnL4AhvXjJy1fhUCda6IOSlKUS1Evur9+8fZfwfhpM2tXKEpQ5uBmt1VzhSmZ+cIdJu9ooM3KDkjUHo7otPK2HI5K9jR0jKyRjsDPaa7qiA0UrB0YeWNZiwcx6ehZyEPOhgMtR0gA58xo4YeRvRmcjExdI9ncxM0JIAwBddigjPGjDmQAAACV0RVh0ZGF0ZTpjcmVhdGUAMjAxNy0wMy0zMFQxMzowMzoxMSswMjowMORKSbgAAAAldEVYdGRhdGU6bW9kaWZ5ADIwMTctMDMtMzBUMTM6MDM6MTErMDI6MDCVF/EEAAAARnRFWHRzb2Z0d2FyZQBJbWFnZU1hZ2ljayA2LjcuOC05IDIwMTYtMDYtMTYgUTE2IGh0dHA6Ly93d3cuaW1hZ2VtYWdpY2sub3Jn5r80tgAAABh0RVh0VGh1bWI6OkRvY3VtZW50OjpQYWdlcwAxp/+7LwAAABh0RVh0VGh1bWI6OkltYWdlOjpoZWlnaHQANTEywNBQUQAAABd0RVh0VGh1bWI6OkltYWdlOjpXaWR0aAA1MTIcfAPcAAAAGXRFWHRUaHVtYjo6TWltZXR5cGUAaW1hZ2UvcG5nP7JWTgAAABd0RVh0VGh1bWI6Ok1UaW1lADE0OTA4NzE3OTF28+iMAAAAE3RFWHRUaHVtYjo6U2l6ZQAyOS40S0JCC+gWuwAAAFB0RVh0VGh1bWI6OlVSSQBmaWxlOi8vLi91cGxvYWRzL2Nhcmxvc3ByZXZpL3NZcDlhM3kvMTE5NS8xNDkwODg5NjQ2LWJ1ZmZlcl84MjUyMy5wbmf+f3XHAAAAAElFTkSuQmCC"
+from tkinter import *
+from bufeer import Buff
+#pyinstaller -F -w -i="buff.ico" -n="SmartBuffertest" --collect-all pynput main.py
+class App():
+
+    def __init__(self):
+        self.windows = Tk()
+        self.windows.title("SmartBuffer")
+        self.windows.resizable(0, 0)
+        self.windows.tk.call("wm", "iconphoto", self.windows._w, PhotoImage(data=icon))
+        self.build()
+        self.smart = Buff()
+
+        self.windows.minsize(250,380)
+        self.windows.mainloop()
+
+    def start(self):
+        self.smart.AddHotKey()
+        self.lblApp.configure(text = "Работает")
+        print("start")
+
+    def stop(self):
+        self.smart.RemoveHotKey()
+        self.lblApp.configure(text = "Не работает")
+        print("stop")
+
+    def acceptBuff3(self):
+        buff3 = self.entBuff3.get()
+        buff3K = self.entBuff3k.get()
+        try:
+            whait = float(self.entWhait.get())
+        except:
+            whait = 0.5
+
+        self.smart.RemapHotKey(hotkey4 = buff3K)
+        self.smart.buff2 = buff3
+        self.smart.whait = whait
+        self.lblBuff3.configure(text = 'Буфер №3    "."\n{}'.format(buff3))
+        self.lblWhait.configure(text = 'Сек: {}'.format(whait))
+        self.lblBuff3Hot.configure(text = buff3K)
+
+    def acceptBuff(self):
+        copy =  self.entCopy.get()
+        past =  self.entPast.get()
+        buff1 = self.entBuff1.get()
+        buff2 = self.entBuff2.get()
+
+        self.smart.buff0 = buff1
+        self.smart.buff1 = buff2
+
+        self.smart.RemapHotKey(hotkey = past, hotkey2 = copy)
+        self.lblPast.configure(text = past)
+        self.lblCopy.configure(text = copy)
+        self.lblBuff1.configure(text = "Буфер №1\n"+buff1)
+        self.lblBuff2.configure(text = "Буфер №2\n"+buff2)
+        print("acceptBuff")
+
+    def acceptMouse(self):
+        mouse = self.entMouse.get()
+        self.smart.RemapHotKey(hotkey3 = mouse)
+        self.lblMousHot.configure(text = mouse)
+        print("acceptMouse")
+
+    def mouseCordinats(self):
+        print("mouseCordinats")
+        self.smart.Mouse()
+        self.lblMouseCord.configure(text = "Cord {} : {}".format(self.smart.x, self.smart.y))
+
+
+    def build(self):
+        window = Frame(self.windows, borderwidth = 11)
+        window.grid(row = 1, column = 1, padx = 5, pady = 5)
+
+        self.lblBuff1 = Label(window, text = 'Буфер №1\nNone')
+        self.entBuff1 = Entry(window)
+        self.lblBuff2 = Label(window, text = 'Буфер №2\nNone')
+        self.entBuff2 = Entry(window)
+
+        self.lblPastEnt = Label(window, text = "Быстрый ввод №1", anchor = 'w')
+        self.entPast = Entry(window)
+        self.lblPast = Label(window, text = " ")
+
+        self.lblCopyEnt = Label(window, text = "Быстрый ввод №2", anchor = 'w')
+        self.entCopy = Entry(window)
+        self.lblCopy = Label(window, text = " ")
+
+        self.btnAceptBuff = Button(window, text = "Приниять", command = self.acceptBuff)
+
+        self.frmMid = Frame(window)
+
+        self.btnMouseCord = Button(window, text = "Задать кординаты", command = self.mouseCordinats)
+        self.lblMouseCord = Label(window, text = "Cord 0 : 0", width = 12, anchor = "center")
+
+        self.lblMouse = Label(window, text = "Нажатие", anchor = 'w')
+        self.entMouse = Entry(window)
+        self.lblMousHot = Label(window, text = " ")
+
+        self.btnAceptMouse = Button(window, text = "Принять", command = self.acceptMouse)
+
+        self.frmMid2 = Frame(window)
+
+        self.lblBuff3 = Label(window, text = 'Буфер №3    "."\nNone')
+        self.entBuff3 = Entry(window)
+
+        self.lblBuff3k = Label(window, text = "Быстрый ввод №3", anchor = 'w')
+        self.entBuff3k = Entry(window)
+        self.lblBuff3Hot = Label(window, text = " ")
+        self.entWhait = Entry(window)
+        self.lblWhait = Label(window, text = "Сек: 0.5")
+        self.btnAceptBuff3 = Button(window, text = "Принять", command = self.acceptBuff3)
+
+        self.frmMid3 = Frame(window)
+
+        self.btnAppStart = Button(window, text = "Старт", command = self.start)
+        self.btnAppStop = Button(window, text = "Стоп", command = self.stop)
+        self.lblApp = Label(window, text = "Не работает")
+
+
+
+        self.lblBuff1.grid(row = 1, column = 1, pady=1, padx=1, sticky="nsew")
+        self.entBuff1.grid(row = 2, column = 1, pady=1, padx=1, sticky="nsew")
+        self.lblBuff2.grid(row = 1, column = 2, pady=1, padx=1, sticky="nsew")
+        self.entBuff2.grid(row = 2, column = 2, pady=1, padx=1, sticky="nsew")
+
+        self.lblPastEnt.grid(row = 3, column = 1, pady=1, padx=1, sticky="nsew")
+        self.entPast.grid(row = 4, column = 1, pady=1, padx=1, sticky="nsew")
+        self.lblPast.grid(row = 5, column = 1, pady=1, padx=1, sticky="nsew")
+
+        self.lblCopyEnt.grid(row = 3, column = 2, pady=1, padx=1, sticky="nsew")
+        self.entCopy.grid(row = 4, column = 2, pady=1, padx=1, sticky="nsew")
+        self.lblCopy.grid(row = 5, column = 2, pady=1, padx=1, sticky="nsew")
+
+        self.btnAceptBuff.grid(row = 7, column = 1, columnspan = 2, pady=1, padx=1, sticky="nsew")
+
+        self.frmMid.grid(row = 8, column = 1, pady=15, padx=0, sticky="nsew")
+
+        l = 8
+        self.btnMouseCord.grid(row = 9+l, column = 1, pady=1, padx=1, sticky="nsew")
+        self.lblMouseCord.grid(row = 9+l, column = 2, pady=1, padx=1, sticky="nsew")
+
+        self.lblMouse.grid(row = 10+l, column = 1, pady=1, padx=1, sticky="nsew")
+        self.entMouse.grid(row = 11+l, column = 1, pady=1, padx=1, sticky="nsew")
+        self.lblMousHot.grid(row = 11+l, column = 2, pady=1, padx=1, sticky="nsew")
+
+        self.btnAceptMouse.grid(row = 12+l, column = 1, pady=1, padx=1, sticky="nsew")
+
+        self.frmMid2.grid(row = 16, column = 1, pady=15, padx=0, sticky="nsew")
+
+        l1 = -4
+        self.lblBuff3.grid(row = 14+l1, column = 1, columnspan = 2, pady=1, padx=1, sticky="nsew")
+        self.entBuff3.grid(row = 15+l1, column = 1, columnspan = 2, pady=1, padx=1, sticky="nsew")
+
+        self.lblBuff3k.grid(row = 16+l1, column = 1, pady=1, padx=1, sticky="nsew")
+        self.entBuff3k.grid(row = 17+l1, column = 1, pady=1, padx=1, sticky="nsew")
+        self.lblBuff3Hot.grid(row = 17+l1, column = 2, pady=1, padx=1, sticky="nsew")
+        self.entWhait.grid(row = 18+l1, column = 1, pady=1, padx=1, sticky="nsew")
+        self.lblWhait.grid(row = 18+l1, column = 2, pady=1, padx=1, sticky="nsew")
+        self.btnAceptBuff3.grid(row = 19+l1, column = 1, columnspan = 2, pady=1, padx=1, sticky="nsew")
+
+        self.frmMid3.grid(row = 30, column = 1, pady=15, padx=0, sticky="nsew")
+
+        self.btnAppStart.grid(row = 31, column = 1, pady=1, padx=1, columnspan = 2, sticky="nsew")
+        self.btnAppStop.grid(row = 32, column = 1, pady=1, padx=1, columnspan = 2, sticky="nsew")
+        self.lblApp.grid(row = 33, column = 1, pady=1, padx=1, columnspan = 2, sticky="nsew")
+
+App()
